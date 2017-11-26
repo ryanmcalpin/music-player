fbda1ed89d5a3a11b2064af112b5c6309bf506af
Ryan McAlpin <rhmcalpin@gmail.com>
2017-11-25 20:05:57 -0800

    convert to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Howl } from 'howler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  songPaths = ["assets/A1 Wake_320kbps.mp3", "assets/A2 Rorschach_320kbps.mp3", "assets/A3 Empiricist_320kbps.mp3", "assets/A4 Algernon_320kbps.mp3"];
+
+  songs = [];
+  playingIndex = undefined;
+
+  ngOnInit() {
+    // this.songPaths.forEach(function(path) {
+    //   let song = new Howl({
+    //     src: [path]
+    //   })
+    //   this.songs.push(song)
+    // })
+
+
+    for (let i = 0; i < this.songPaths.length; i++) {
+      let song = new Howl({
+        src: [this.songPaths[i]],
+      });
+      this.songs.push(song);
+    }
+
+  }
+
+
+  playSong(i){
+    if (this.playingIndex !== undefined) this.songs[this.playingIndex].stop();
+    this.playingIndex = i;
+    this.songs[i].play();
+  }
 }
